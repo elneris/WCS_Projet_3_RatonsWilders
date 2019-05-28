8f02eb22b6c717e9849364bbf92fd5c2db02e89b
Pascal Encinas <krionari@gmail.com>
2019-05-28 20:13:33 +0200

    fix table style

---
+++ src/Migrations/Version20190528175206.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190528175206 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection
+                ->getDatabasePlatform()
+                ->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE activity DROP FOREIGN KEY FK_AC74095A6BF700BD');
+        $this->addSql('CREATE TABLE style (id INT AUTO_INCREMENT NOT NULL,
+                            type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) 
+                            DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE status');
+        $this->addSql('DROP INDEX IDX_AC74095A6BF700BD ON activity');
+        $this->addSql('ALTER TABLE activity CHANGE status_id style_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE activity ADD CONSTRAINT FK_AC74095ABACD6074 
+                            FOREIGN KEY (style_id) REFERENCES style (id)');
+        $this->addSql('CREATE INDEX IDX_AC74095ABACD6074 ON activity (style_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE activity DROP FOREIGN KEY FK_AC74095ABACD6074');
+        $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL,
+                            type VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) 
+                            DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE style');
+        $this->addSql('DROP INDEX IDX_AC74095ABACD6074 ON activity');
+        $this->addSql('ALTER TABLE activity CHANGE style_id status_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE activity ADD CONSTRAINT FK_AC74095A6BF700BD 
+                            FOREIGN KEY (status_id) REFERENCES status (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_AC74095A6BF700BD ON activity (status_id)');
+    }
+}
