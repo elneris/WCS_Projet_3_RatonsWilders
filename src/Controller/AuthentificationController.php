9a11bfb8b7715d793e1ce3b213bd5b9cebf8e96c
Maxime Vasseur <zertouflex@gmail.com>
2019-05-29 09:26:50 +0200

    arborescence sans le CRUD

---
+++ src/Controller/AuthentificationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthentificationController extends AbstractController
+{
+    /**
+     * @Route("/", name="authentification")
+     */
+    public function index()
+    {
+        return $this->render('authentification.html.twig');
+    }
+}
