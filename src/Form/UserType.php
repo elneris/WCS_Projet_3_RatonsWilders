474b3a6c7459bad8dbaf54487cbb99257ef253f3
Maxime Vasseur <zertouflex@gmail.com>
2019-06-05 14:12:33 +0200

    changement des label en fr

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,8 +2,18 @@
 
 namespace App\Form;
 
+use App\Entity\Domain;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,21 +22,71 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('artistName')
-            ->add('email')
-            ->add('phoneNumber')
-            ->add('city')
-            ->add('posteCode')
-            ->add('birthdate')
-            ->add('address')
-            ->add('password')
+            ->add('firstname',TextType::class, [
+
+                'label' => 'Prénom'
+            ])
+
+            ->add('lastname',TextType::class, [
+
+                'label' => 'Nom'
+            ])
+            ->add('artistName',TextType::class,[
+
+                'label'=>"Nom d'artiste"
+            ])
+
+            ->add('email',EmailType::class)
+
+            ->add('phoneNumber',TelType::class,[
+
+                'label' => 'N° de téléphone'
+            ])
+
+            ->add('city',CountryType::class,[
+
+                'label'=>'Ville'
+            ])
+
+            ->add('posteCode',NumberType::class, [
+
+                'label'=>'Code postal'
+            ])
+
+            ->add('birthdate',BirthdayType::class,[
+
+                'label'=>'Date de naissance'
+            ])
+
+            ->add('address',TextType::class,[
+
+                'label'=>'Adresse'
+            ])
+
+            ->add('password',PasswordType::class, [
+
+                'label' =>'Mot de passe'
+            ])
+
             ->add('geoArea')
-            ->add('price')
-            ->add('about')
+
+            ->add('price', MoneyType::class, [
+
+                'divisor' => 100,
+                'label' => 'Tarif',
+            ])
+
+            ->add('about',TextType::class,[
+                'label'=>'Description'
+            ])
+
             ->add('role')
-        ;
+            ->add('activities', EntityType::class, [
+
+                'label' => 'Domaine',
+                'class' => Domain::class,
+                'choice_label' => 'name'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
