08d6544f405850e247da977fbefc4e65e44f928a
Maxime Vasseur <zertouflex@gmail.com>
2019-06-17 11:26:36 +0200

    issue ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,14 +22,14 @@ class UserController extends AbstractController
 {
 
     /**
-     * @Route("/", name="user_index", methods={"GET"})
-     * @param UserRepository $userRepository
+     * @Route("/{id}", name="user_index", methods={"GET"})
+     * @param User $user
      * @return Response
      */
-    public function index(UserRepository $userRepository): Response
+    public function index(User $user): Response
     {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
         ]);
     }
 
@@ -59,15 +59,12 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="user_show", methods={"GET"})
-     * @param User $user
+     * @Route("/", name="user_show")
      * @return Response
      */
-    public function show(User $user): Response
+    public function show(): Response
     {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
+        return $this->render('user/show.html.twig');
     }
 
     /**
@@ -150,22 +147,5 @@ class UserController extends AbstractController
             'user' => $user,
             'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("/{id}", name="user_delete", methods={"DELETE"})
-     * @param Request $request
-     * @param User $user
-     * @return Response
-     */
-    public function delete(Request $request, User $user): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($user);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('user_index');
     }
 }
