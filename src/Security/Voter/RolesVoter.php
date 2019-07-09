a29689b52b67aa70a4998d60405f41f7d0949da6
Elneris Dang <elneris.dang@gmail.com>
2019-07-09 15:46:29 +0200

    ajout de voter et refactor du code show

---
+++ src/Security/Voter/RolesVoter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class RolesVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, ['ONLY_USER'])
+            && $subject instanceof User;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'ONLY_USER':
+                $result = true;
+                foreach ($user->getRoles() as $role) {
+                    if (($role === 'ROLE_ADMIN')) {
+                        $result = false;
+                        break;
+                    }
+                }
+                return $result;
+                break;
+        }
+
+        return false;
+    }
+}
