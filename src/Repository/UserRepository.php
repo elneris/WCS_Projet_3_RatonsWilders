59c41f04df9447770299b2418aa2f178a7855a9a
Maxime Vasseur <zertouflex@gmail.com>
2019-07-11 18:12:04 +0200

    filtre

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -52,7 +52,6 @@ class UserRepository extends ServiceEntityRepository
         if ($filters['zone']) {
             $qb->andWhere('user.geoArea = :geoArea')
                 ->setParameter("geoArea", $filters['zone']);
-
         }
 
         if ($filters['searchField']) {
