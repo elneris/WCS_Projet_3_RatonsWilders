c59006a6a11c6a3afc74e583a470c08090918634
Maxime Vasseur <zertouflex@gmail.com>
2019-07-11 17:33:47 +0200

    filtre par rzone d'action

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\Domain;
 use App\Entity\Skill;
 use App\Entity\Style;
+use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -15,6 +17,20 @@ class SearchType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('zone', ChoiceType::class, [
+                'label' => 'Zone de déplacement',
+                'choices'  => [
+                    'Tous' => null,
+                    'Région Nord' => 'Région Nord',
+                    'Région Nord Ouest' => 'Région Nord Ouest',
+                    'Région Nord Est' => 'Région Nord Est',
+                    'Région Centre' => 'Région Centre',
+                    'Région Sud' => 'Région Sud',
+                    'Région Sud Ouest' => 'Région Sud Ouest',
+                    'Région Sud Est' => 'Région Sud Est',
+
+
+                ]])
             ->add(
                 'metier',
                 EntityType::class,
